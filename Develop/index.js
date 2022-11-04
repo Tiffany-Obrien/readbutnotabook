74a29728f12a7e28b12eae6074da0b48e6376c63
Tiffany-Obrien <105178688+Tiffany-Obrien@users.noreply.github.com>
2022-11-04 16:45:02 -0400

    Add files via upload

---
+++ Develop/index.js
@@ -0,0 +1,36 @@
+// TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function getLicense(value) {
+if (value)
+    return
+}
+
+
+function validateInput(value) {
+    if (value!='');
+    return true;
+}
+// TODO: Create a function to initialize app
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+}
+
+
+// Function call to initialize app
+function init() {
+    inquirer.prompt(questions).then((data) => {
+        console.log(JSON.stringify(data, null, " "));
+        data.getLicense = getLicense(data.license);
+        writeToFile("./example/README.md", data);
+    });
+}
